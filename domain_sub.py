bf5cc0ea05c5e47a9052678e8385d7f2cc890039
mahoo12138 <mahoo12138@qq.com>
2020-05-01 14:57:07 +0800

    add path check

--- domain_sub.py
+++ domain_sub.py
@@ -9,19 +9,21 @@ def file_name(file_dir):
         print(files)  # 2-当前路径下所有非目录子文件(list)
 
 
-for files in os.walk('D:/blog-with-hexo/source/_posts/'):
-    print('当前目录下共有{}篇文章\n'.format(len(files[2])))
-# file_name('D:/Mahoo/BLog/source/_posts/')
-
-
-for i in range(len(files[2])):
-    f = open('D:/blog-with-hexo/source/_posts/' + files[2][i], 'r', encoding="utf-8")
-    string = f.read()
-    f.close()
-    key = 'http://img.mahoo12138.cn/'  # 旧域名
-    new = 'https://img.mahoo12138.cn/'  # 新域名
-    string = re.sub(key, new, string)
-    f = open('D:/blog-with-hexo/source/_posts/' + files[2][i], 'w', encoding="utf-8")
-    f.write(string)
-    f.close()
-    print('第{}篇操作完毕\n'.format(i))
+# 注意，文件地址后的 ‘ / ’不能少
+path = 'D:/Website/blog-with-hexo/source/_posts/'
+if not os.path.exists(path):
+    print("请检查路径是否正确！")
+else:
+    for files in os.walk(path):
+        print('当前目录下共有{}篇文章\n'.format(len(files[2])))
+        for i in range(len(files[2])):
+            f = open(path + files[2][i], 'r', encoding="utf-8")
+            string = f.read()
+            f.close()
+            old = 'https://cdn.jsdelivr.net/gh/mahoo12138/js-css-cdn/hexo-images/Study/'  # 旧域名
+            new = 'https://cdn.jsdelivr.net/gh/mahoo12138/js-css-cdn/hexo-images/study/'  # 新域名
+            string = re.sub(old, new, string)
+            f = open(path + files[2][i], 'w', encoding="utf-8")
+            f.write(string)
+            f.close()
+            print('第{}篇操作完毕\n'.format(i))
